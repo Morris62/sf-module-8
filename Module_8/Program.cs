b7872bc5db1e3e06f75c05182ab539edc9b58a7d
a.chaturov <chaturovai@gmail.com>
2025-02-22 00:58:49 +0300

    unit 8.4.1

--- Module_8/Program.cs
+++ Module_8/Program.cs
@@ -4,50 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        string tempFile = Path.GetTempFileName();
-        var fileInfo = new FileInfo(tempFile);
-        
-        using (var sw = fileInfo.CreateText())
-        {
-            sw.WriteLine("Олег");
-            sw.WriteLine("Дмитрий");
-            sw.WriteLine("Иван");
-        }
-        
-        using (var sr = fileInfo.OpenText())
-        {
-            string? line = "";
-            while ((line = sr.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
-        }
+        var file = @"/Users/a.chaturov/BinaryFile.bin";
 
-        try
+        if (File.Exists(file))
         {
-            var tempFile2 = Path.GetTempFileName();
-            var fileInfo2 = new FileInfo(tempFile2);
-            fileInfo2.Delete();
-            fileInfo.CopyTo(tempFile2);
-            Console.WriteLine($"Файл {tempFile} скопирован в {tempFile2}");
-            fileInfo.Delete();
-            Console.WriteLine($"Файл {tempFile} удален");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        var fileProgram = @"/Users/a.chaturov/RiderProjects/learning/SkillFactory/Module_8/Module_8/Program.cs";
-        using (var sr = File.OpenText(fileProgram))
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            string? line = "";
-            while ((line = sr.ReadLine()) != null)
+            using (var br = new BinaryReader(File.Open(file, FileMode.Open)))
             {
-                Console.WriteLine(line);
+                string str = br.ReadString();
+                Console.WriteLine($"{str}");
             }
-            Console.ResetColor();
         }
     }
 }
