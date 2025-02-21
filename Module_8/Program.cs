47f9eebe70fd32bc7c54c1d36affd11634d0e83b
a.chaturov <chaturovai@gmail.com>
2025-02-22 01:24:57 +0300

    unit 8.4.3

--- Module_8/Program.cs
+++ Module_8/Program.cs
@@ -1,11 +1,51 @@
-﻿namespace Module_8;
+﻿using System.Text.Json;
+
+namespace Module_8;
 
 class Program
 {
+    private static readonly string file = @"/Users/a.chaturov/BinaryFile.bin";
+    
+    [Serializable]
+    class Pet
+    {
+        public string Name{ get; set;}
+        public int Age { get; set; }
+        public Pet(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    
     static void Main(string[] args)
     {
-        var file = @"/Users/a.chaturov/BinaryFile.bin";
+        WriteValues();
+        ReadValues();
 
+        var pet = new Pet(name: "Max", age: 3);
+        // сериализация
+        var options = new JsonSerializerOptions{WriteIndented = true};
+        var jsonString = JsonSerializer.Serialize(pet, options);
+        File.WriteAllText("myPets.json", jsonString);
+        Console.WriteLine("Объект сериализован");
+        
+        jsonString = File.ReadAllText("myPets.json");
+        var newPet = JsonSerializer.Deserialize<Pet>(jsonString);
+        Console.WriteLine("Объект десериализован");
+
+        Console.WriteLine($"Имя {newPet.Name} - возраст {newPet.Age}");
+
+    }
+    private static void WriteValues()
+    {
+        using (var bw = new BinaryWriter(File.Open(file, FileMode.OpenOrCreate)))
+        {
+            bw.Write($"Файл изменен {DateTime.Now} на компьютере {Environment.OSVersion}");
+        }
+    }
+    private static void ReadValues()
+    {
         if (File.Exists(file))
         {
             using (var br = new BinaryReader(File.Open(file, FileMode.Open)))
