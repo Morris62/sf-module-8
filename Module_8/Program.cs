88302a190dbb882827b0f19773fcbb601a523cfb
a.chaturov <chaturovai@gmail.com>
2025-02-21 23:11:41 +0300

    unit 8.3.1

--- Module_8/Program.cs
+++ Module_8/Program.cs
@@ -4,111 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        var drives = DriveInfo.GetDrives();
-
-        foreach (var drive in drives)
+        string tempFile = Path.GetTempFileName();
+        var fileInfo = new FileInfo(tempFile);
+        
+        using (var sw = fileInfo.CreateText())
         {
-            Console.WriteLine($"Название: {drive.Name}");
-            Console.WriteLine($"Тип: {drive.DriveType}");
-            Console.WriteLine($"Формат: {drive.DriveFormat}");
-
-            if (drive.IsReady)
-            {
-                Console.WriteLine($"Метка: {drive.VolumeLabel}");
-                Console.WriteLine($"Объем: {drive.TotalSize}");
-                Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
-                Console.WriteLine($"Доступно: {drive.AvailableFreeSpace}");
-            }
-
-            Console.Write(Environment.NewLine);
+            sw.WriteLine("Олег");
+            sw.WriteLine("Дмитрий");
+            sw.WriteLine("Иван");
         }
-
-        GetCatalogs();
+        
+        using (var sr = fileInfo.OpenText())
+        {
+            string? line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
 
         try
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"/test");
-            if (!dirInfo.Exists)
-            {
-                dirInfo.Create();
-            }
-
-            dirInfo.CreateSubdirectory("NewFolder");
+            var tempFile2 = Path.GetTempFileName();
+            var fileInfo2 = new FileInfo(tempFile2);
+            fileInfo2.Delete();
+            fileInfo.CopyTo(tempFile2);
+            Console.WriteLine($"Файл {tempFile} скопирован в {tempFile2}");
+            fileInfo.Delete();
+            Console.WriteLine($"Файл {tempFile} удален");
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
 
-        GetCatalogs();
-        
-        DirectoryInfo dirInfo2 = new DirectoryInfo(@"/Users/a.chaturov/test");
-        if (!dirInfo2.Exists)
+        var fileProgram = @"/Users/a.chaturov/RiderProjects/learning/SkillFactory/Module_8/Module_8/Program.cs";
+        using (var sr = File.OpenText(fileProgram))
         {
-            dirInfo2.Create();
-        }
-        
-        dirInfo2.CreateSubdirectory("NewFolder1");
-        dirInfo2.CreateSubdirectory("NewFolder2");
-
-        Console.WriteLine($"Название каталога: {dirInfo2.Name}");
-        Console.WriteLine($"Полное название каталога: {dirInfo2.FullName}");
-        Console.WriteLine($"Время создания каталога: {dirInfo2.CreationTime}");
-        
-        var path1 = @"/Users/a.chaturov/test/NewFolder1";
-        var path2 = @"/Users/a.chaturov/test/NewFolder2/NewFolder1";
-        
-        DirectoryInfo dirInfo3 = new DirectoryInfo(path1);
-        if (dirInfo3.Exists && !Directory.Exists(path2))
-        {
-            dirInfo3.MoveTo(path2);
-        }
-        
-        try
-        {
-            dirInfo2.Delete(true);
-            Console.WriteLine($"Каталог {dirInfo2.FullName} удален");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-    }
-
-    private static void GetCatalogs()
-    {
-        string path = @"/";
-
-        try
-        {
-            if (Directory.Exists(path))
+            Console.ForegroundColor = ConsoleColor.Green;
+            string? line = "";
+            while ((line = sr.ReadLine()) != null)
             {
-                Console.WriteLine("Папки:");
-                var dirs = Directory.GetDirectories(path);
-                foreach (var dir in dirs)
-                {
-                    Console.WriteLine(dir);
-                }
-
-                Console.Write(Environment.NewLine);
-
-                Console.WriteLine("Файлы:");
-                var files = Directory.GetFiles(path);
-                foreach (var file in files)
-                {
-                    Console.WriteLine(file);
-                }
-
-                Console.Write(Environment.NewLine);
-
-                Console.WriteLine($"Количество: {dirs.Length + files.Length}");
-                
-                Console.Write(Environment.NewLine);
+                Console.WriteLine(line);
             }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
+            Console.ResetColor();
         }
     }
 }
